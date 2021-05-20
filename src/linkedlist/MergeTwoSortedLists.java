160e9378b7eebefe765767df5ceb946e975d7344
ray-ux <zlw20010124@163.com>
2021-05-21 00:14:12 +0800

    合并两个有序链表

---
+++ src/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+/**
+ * @author 张烈文
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode<Integer> mergeTwoLists(ListNode<Integer> l1, ListNode<Integer> l2) {
+
+        ListNode res = new ListNode();
+        ListNode tmp = res;
+        while (l1 != null && l2 != null) {
+            if (l1.data > l2.data) {
+                tmp.next = l2;
+                l2 = l2.next;
+            } else {
+                tmp.next = l1;
+                l1 = l1.next;
+            }
+            tmp = tmp.next;
+        }
+
+        if (l1 != null) {
+            tmp.next = l1;
+        }
+        if (l2 != null) {
+            tmp.next = l2;
+        }
+
+        return res.next;
+    }
+}
