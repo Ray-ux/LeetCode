e3a2721338d1a3e848e8a81c6d6789b46c4c49de
ray-ux <zlw20010124@163.com>
2021-06-06 15:40:24 +0800

    一和零

--- src/dynamic/OnesAndZeroes.java
+++ src/dynamic/OnesAndZeroes.java
@@ -1,5 +1,7 @@
 package dynamic;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,6 +11,13 @@ import java.util.List;
  */
 public class OnesAndZeroes {
 
+    /**
+     * 思路一：动态规划：背包问题0-1
+     * @param strs
+     * @param m
+     * @param n
+     * @return
+     */
     public int findMaxForm(String[] strs, int m, int n) {
         int len = strs.length;
         int[][][] dp = new int[len + 1][m + 1][n + 1];
@@ -36,6 +45,50 @@ public class OnesAndZeroes {
             cnt[c - '0']++;
         }
         return cnt;
+    }
+
+    /**
+     * 思路二：回溯超时
+     * 先找出所有子集在遍历子集找出0和1的数量
+     * @param strs
+     * @param m
+     * @param n
+     * @return
+     */
+    public int findMaxForm1(String[] strs, int m, int n) {
+        int maxLen = 0;
+        List<List<String>> res = new ArrayList<>();
+        Deque<String> path = new LinkedList<>();
+        dfs(0, strs, res, path);
+
+        for (List<String> inner : res) {
+            int one = 0;
+            int zero = 0;
+            for (String str : inner) {
+                for (int i = 0; i < str.length(); i++) {
+                    int num = str.charAt(i) - '0';
+                    if (num == 0) {
+                        zero++;
+                    }else{
+                        one++;
+                    }
+                }
+            }
+            if (zero <= m && one <= n && inner.size() > maxLen) {
+                maxLen = inner.size();
+            }
+        }
+        return maxLen;
+    }
+
+    private void dfs(int begin, String[] strs, List<List<String>> res, Deque<String> path) {
+
+        for (int i = begin; i < strs.length; i++) {
+            path.addLast(strs[i]);
+            res.add(new ArrayList<>(path));
+            dfs(i + 1, strs, res, path);
+            path.removeLast();
+        }
     }
 
 }
