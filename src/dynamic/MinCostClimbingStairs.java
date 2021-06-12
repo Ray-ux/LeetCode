faa02180613fb933ca8d995a0a7ee94a29d891ea
ray-ux <zlw20010124@163.com>
2021-06-12 15:35:59 +0800

    使用最小花费爬楼梯

--- src/dynamic/MinCostClimbingStairs.java
+++ src/dynamic/MinCostClimbingStairs.java
@@ -9,9 +9,7 @@ public class MinCostClimbingStairs {
 
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
-        if(n<=1){
-            return  cost[0];
-        }
+
 //        1.确定dp数组，dp[i]：代表爬上第i级台阶所花费的最小体力
 //        2.确定递推公式：dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i],第i级所花费的最小体力等于i-1级和i-2级的台阶花费体力的最小值+第i级需要花费的体力
         int[] dp=new int[n];
