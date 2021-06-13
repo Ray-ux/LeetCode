7df71432d8002b4f4085ebe5a73abbcaefcd372a
ray-ux <zlw20010124@163.com>
2021-06-13 15:43:50 +0800

    不同路径Ⅱ

--- README.md
+++ README.md
@@ -113,4 +113,4 @@
 [爬楼梯](https://github.com/Ray-ux/LeetCode/blob/master/src/dynamic/ClimbingStairs.java) </br>
 [使用最小花费爬楼梯](https://github.com/Ray-ux/LeetCode/blob/master/src/dynamic/MinCostClimbingStairs.java) </br>
 [不同路径](https://github.com/Ray-ux/LeetCode/blob/master/src/dynamic/UniquePaths.java) </br>
-[]() </br>
+[不同路径Ⅱ](https://github.com/Ray-ux/LeetCode/blob/master/src/dynamic/UniquePathsⅡ.java) </br>
---
+++ src/dynamic/UniquePathsⅡ.java
@@ -0,0 +1,48 @@
+package dynamic;
+
+/**
+ * 不同路径Ⅱ
+ *
+ * @author 张烈文
+ */
+public class UniquePathsⅡ {
+
+    /**
+     * 该题和不同路径有异曲同工之处：只不过多了障碍物，我们只需要保证在有障碍物的位置dp[i][j]=0就可以啦
+     * 具体思路和上一题一样
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public  int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+//        初始化：首行，遇到障碍物时，障碍物之处和首行之后的位置都为0
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+//        初始化：首列，遇到障碍物时，障碍物之处和首列之后的位置都为0
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            }
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+//                只有在没有障碍物时，才保存状态，有障碍的位置处为0
+                if (obstacleGrid[i][j] != 1) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+            return dp[m - 1][n - 1];
+    }
+
+
+}
