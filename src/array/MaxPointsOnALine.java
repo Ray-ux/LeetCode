96af052866d06f324c1396db7c17532505cd6253
ray-ux <zlw20010124@163.com>
2021-06-24 11:18:51 +0800

    直线上最多的点数

--- src/array/MaxPointsOnALine.java
+++ src/array/MaxPointsOnALine.java
@@ -8,6 +8,9 @@ public class MaxPointsOnALine {
 
     /**
      *
+     * 思路：尝试先将两个点构成一条直线，然后检查其余点是否落在该直线中
+     * 时间复杂度：O(n^3)
+     * 每层循环确定一个点
      * @param ps
      * @return
      */
@@ -21,6 +24,8 @@ public class MaxPointsOnALine {
                 int cnt = 2;
                 for (int k = j + 1; k < n; k++) {
                     int[] p = ps[k];
+//                    这里实际采用了相似直线的两点公式来确两个点是否在同一条直线上，然后在对角相乘即可
+//                    具体推到可以考虑使用相似三角形：画图分析即可，
                     int s1 = (y[1] - x[1]) * (p[0] - y[0]);
                     int s2 = (p[1] - y[1]) * (y[0 ] - x[0]);
                     if (s1 == s2) {
