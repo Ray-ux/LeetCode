1133690b7bd56510359b1a3b47cbcbc18931d0bb
Ray-ux <zlw20010124@163.com>
2021-10-01 22:55:43 +0800

    :rocket:实现str()

--- src/string/ImplementStr.java
+++ src/string/ImplementStr.java
@@ -6,5 +6,47 @@ package string;
  */
 public class ImplementStr {
 
+    /**
+     * 虽然此题可以用已经实现好的api来做
+     * 但是为了锻炼kmp算法，因此还是写一遍
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        int len = needle.length();
+        if (len == 0) {
+            return 0;
+        }
+        int[] next = new int[len];
+        getNext(next, needle);
+        int j = -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j >= 0 && haystack.charAt(i) != needle.charAt(j + 1)) {
+                j = next[j];
+            }
+            if (haystack.charAt(i) == needle.charAt(j + 1)) {
+                j++;
+            }
+            if (j == needle.length() - 1) {
+                return (i - needle.length() + 1);
+            }
+        }
+        return -1;
 
+    }
+
+    private void getNext(int[] next,String s) {
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < s.length(); i++) {
+            while (j >= 0 && s.charAt(i) != s.charAt(j + 1)) {
+                j = next[j];
+            }
+            if (s.charAt(i) == s.charAt(j + 1)) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
 }
