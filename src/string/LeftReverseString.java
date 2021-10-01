81c10470a61f92a1c8769325d6684ee5a82e38a9
Ray-ux <zlw20010124@163.com>
2021-10-01 21:26:40 +0800

    :construction:左翻转字符串

--- src/string/LeftReverseString.java
+++ src/string/LeftReverseString.java
@@ -7,21 +7,57 @@ package string;
  * @date 2021/10/1 21:01
  */
 public class LeftReverseString {
-    public  static String reverseLeftWords(String s, int n) {
-        int len = s.length();
-        char[] array = s.toCharArray();
-        String substring = s.substring(0, n);
-        int start = 0;
-        for (int i = n; i < len; i++) {
-            array[start++] = array[i];
+
+
+    /**
+     * 自己的思路：
+     * 1.先截取0-n的字符串
+     * 2.然后将n之后字符串向前移
+     * 3.最后再将之前截取的字符串添加到结尾即可
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords1(String s, int n) {
+        int len=s.length();
+        StringBuilder temp=new StringBuilder(s);
+        String str=s.substring(0,n);
+        int start=0;
+        for(int end=n;end<len;end++,start++){
+            temp.setCharAt(start,temp.charAt(end));
         }
-        for (int i = 0; i < substring.length(); i++) {
-            array[start++] = substring.charAt(i);
+        for(int i=0;start<len;i++){
+            temp.setCharAt(start,str.charAt(i));
+            start++;
         }
-        return array.toString();
+        return temp.toString();
+
+    }
+    /**
+     * 反转区间为0-n的字符串
+     * 反转区间n-len-1的字符串
+     * 反转整个字符串
+     *
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder str = new StringBuilder(s);
+        reverse(str, 0, n - 1);
+        reverse(str, n, s.length() - 1);
+        return str.reverse().toString();
     }
 
-    public static void main(String[] args) {
-        reverseLeftWords("abcdefg", 2);
+    private void reverse(StringBuilder str, int start, int end) {
+        while (start < end) {
+            char c = str.charAt(start);
+            str.setCharAt(start, str.charAt(end));
+            str.setCharAt(end, c);
+            start++;
+            end++;
+        }
     }
+
+
 }
