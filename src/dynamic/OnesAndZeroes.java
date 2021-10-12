6a63768fb098a4a3eb4f0b6875c2c13250509cc8
Ray-ux <zlw20010124@163.com>
2021-10-12 19:59:25 +0800

    :rocket:一和零：修改动态规划方法

--- src/dynamic/OnesAndZeroes.java
+++ src/dynamic/OnesAndZeroes.java
@@ -19,32 +19,24 @@ public class OnesAndZeroes {
      * @return
      */
     public int findMaxForm(String[] strs, int m, int n) {
-        int len = strs.length;
-        int[][][] dp = new int[len + 1][m + 1][n + 1];
-
-        for (int i = 1; i <= len; i++) {
-            // 注意：有一位偏移
-            int[] count = countZeroAndOne(strs[i - 1]);
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= n; k++) {
-                    // 先把上一行抄下来
-                    dp[i][j][k] = dp[i - 1][j][k];
-                    int zeros = count[0];
-                    int ones = count[1];
-                    if (j >= zeros && k >= ones) {
-                        dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i - 1][j - zeros][k - ones] + 1);
-                    }
+        int[][] dp = new int[m + 1][n + 1];
+        for (String str : strs) {
+            int oneNums = 0;
+            int zeroNums = 0;
+            for (char c : str.toCharArray()) {
+                if (c == '0') {
+                    zeroNums++;
+                }else{
+                    oneNums++;
+                }
+            }
+            for (int i = m; i >= zeroNums; i--) {
+                for (int j = n; j >= oneNums; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNums][j - oneNums] + 1);
                 }
             }
         }
-        return dp[len][m][n];
-    }
-    private int[] countZeroAndOne(String str) {
-        int[] cnt = new int[2];
-        for (char c : str.toCharArray()) {
-            cnt[c - '0']++;
-        }
-        return cnt;
+        return dp[m][n];
     }
 
     /**
