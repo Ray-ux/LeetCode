76cec24d6111ca10980b264090da71b5aecc973f
Ray-ux <zlw20010124@163.com>
2021-10-25 10:50:32 +0800

    :rocket:调整数组顺序使奇数位于偶数前面

--- src/math/ChangeArrayPosition.java
+++ src/math/ChangeArrayPosition.java
@@ -27,4 +27,25 @@ public class ChangeArrayPosition {
         }
         return nums;
     }
+
+    /**
+     * 双指针法，i代表奇数的个数，j向后遍历数组，若遍历的当前数字为偶数则j++，若为奇数则需要先保存当前奇数，然后再将
+     * [i,j-1]整体向后移动一位，最后再将之前保存的奇数插入到i所指向位置
+     * @param array
+     * @return
+     */
+    public int[] reOrderArray (int[] array) {
+        int i=0;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] % 2 == 1) {
+                int temp = array[j];
+//                [i,j-1]整体向后移动一位，最后再将之前保存的奇数插入到i所指向位置
+                for (int k = j - 1; k >= i; k--) {
+                    array[k + 1] = array[k];
+                }
+                array[i++] = temp;
+            }
+        }
+        return array;
+    }
 }
